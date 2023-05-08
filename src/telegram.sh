b925ec4ca819428b392772522a1dfc1e45908d6c
Loreta Themis <loretath@oa-d5.msk.21-school.ru>
2023-05-08 17:29:30 +0300

    upd telegram

--- src/telegram.sh
+++ src/telegram.sh
@@ -1,8 +1,15 @@
 TELEGRAM_BOT_TOKEN="6008907946:AAGt1PkhRN74cJEN9RenaQlcDVOe3IokpMI"
 TIME=5
 CHAT_ID=637830245
+JOB_STATUS=$1
+if [ $JOB_STATUS -eq 0 ];
+then
+    JOB_STATUS="success"
+else
+    JOB_STATUS="fail"
+fi
 
 URL="https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage"
 
-TEXT="Job: $CI_JOB_NAME%0AStage: $CI_JOB_STAGE%0AStatus: $CI_JOB_STATUS%0A%0AProject:+$CI_PROJECT_NAME%0AURL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG"
+TEXT="Job: $CI_JOB_NAME%0AStage: $CI_JOB_STAGE%0AStatus: $JOB_STATUS%0A%0AProject:+$CI_PROJECT_NAME%0AURL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG"
 curl -s --max-time $TIME -d "chat_id=$CHAT_ID&disable_web_page_preview=1&text=$TEXT" $URL > /dev/null
