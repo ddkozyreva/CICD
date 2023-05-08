8c4118c41b629a736c2e0d1cb524fbe40219f227
Loreta Themis <loretath@oa-d5.msk.21-school.ru>
2023-05-08 17:34:48 +0300

    test fails

--- src/project/grep/grep.sh
+++ src/project/grep/grep.sh
@@ -28,28 +28,28 @@ do
     rm s21_grep*.txt grep*.txt
 done
 
-# for var in -e -i -v -c -l -n #-v -E -T --number-nonblank --number --squeeze-blank
-# do
-#     for test_file1 in $TEST_FILE1 $TEST_FILE2 $TEST_FILE3
-#     do
-#         for test_file2 in $TEST_FILE1 $TEST_FILE2 $TEST_FILE3
-#         do
-#             TEST1="$var $WORD $test_file1 $test_file2"
-#             echo "$TEST1"
-#             ./s21_grep $TEST1 > s21_grep.txt
-#             grep $TEST1 > grep.txt
-#             DIFF_RES="$(diff -s s21_grep.txt grep.txt)"
-#             if [ "$DIFF_RES" = "Files s21_grep.txt and grep.txt are identical" ];
-#             then
-#                 COUNTER_SUCCESS=$(($COUNTER_SUCCESS+1))
-#             else
-#                 echo "$TEST1" >> log.txt
-#                 COUNTER_FAIL=$(($COUNTER_FAIL+1))
-#             fi
-#             rm s21_grep*.txt grep*.txt
-#         done
-#     done
-# done
+for var in -e -i -v -c -l -n #-v -E -T --number-nonblank --number --squeeze-blank
+do
+    for test_file1 in $TEST_FILE1 $TEST_FILE2 $TEST_FILE3
+    do
+        for test_file2 in $TEST_FILE1 $TEST_FILE2 $TEST_FILE3
+        do
+            TEST1="$var $WORD $test_file1 $test_file2"
+            echo "$TEST1"
+            ./s21_grep $TEST1 > s21_grep.txt
+            grep $TEST1 > grep.txt
+            DIFF_RES="$(diff -s s21_grep.txt grep.txt)"
+            if [ "$DIFF_RES" = "Files s21_grep.txt and grep.txt are identical" ];
+            then
+                COUNTER_SUCCESS=$(($COUNTER_SUCCESS+1))
+            else
+                echo "$TEST1" >> log.txt
+                COUNTER_FAIL=$(($COUNTER_FAIL+1))
+            fi
+            rm s21_grep*.txt grep*.txt
+        done
+    done
+done
 
 echo "SUCCESS: $COUNTER_SUCCESS"
 echo "FAIL: $COUNTER_FAIL"
